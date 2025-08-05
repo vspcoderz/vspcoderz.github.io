d13a661a94b05fd4a541f542c16b8791b509d102
Vspcoder <165260098+vspcoderz@users.noreply.github.com>
2025-08-05 15:15:04 +0000

    Hey

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,42 @@
 import "./globals.css";
+import { Space_Grotesk, Space_Mono, Borel } from "next/font/google";
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  variable: "--font-space-grotesk",
+  weight: ["300", "400", "500", "700"], // adjust as needed
+});
+
+const spaceMono = Space_Mono({
+  subsets: ["latin"],
+  variable: "--font-space-mono",
+  weight: ["400", "700"],
+});
+
+const borel = Borel({
+  subsets: ["latin"],
+  variable: "--font-borel",
+  weight: ["400"],
+});
 
 export const metadata = {
-  title:
-    "Vspcoderz Portfolio - Hi, I am Vspcoderz a 10th Student Passionate In Code ",
+  title: "Vspcoderz Portfolio ",
   description:
     "Hey I am Vedant Pakwane AKA Vspcoderz I am In 10th In 2025 I am Intrested In Java , DSA , Javascript , Python , AI/ML , Next.js , React  & More.... ",
+  icons: {
+    icon: "/logo.png",
+    apple: "/logo.png",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-zinc-900 text-zinc-100">{children}</body>
+      <body
+        className={`bg-zinc-900 text-zinc-100 ${spaceGrotesk.variable} ${spaceMono.variable}`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { TextHoverEffect } from "@/components/ui/text-hover-effect";
 
 const HeroSection = () => {
   return (
-    <section className="w-full h-screen ">HeroSection</section>
-  )
-}
+    <>
+      <section className="w-full h-screen flex justify-center items-center">
+        <div className="absolute top-24 left-1/4 w-1/2 h-96 flex items-center justify-center z-0">
+          <TextHoverEffect text="Hello!" />
+        </div>
 
-export default HeroSection
+        <div className="relative z-10  flex flex-col gap-2 items-center">
+          <h1 className="text-8xl font-space-mono">Hi I am Vspcoderz.</h1>
+          <span className="text-8xl font-space-grotesk">Coder. Minecraft</span>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HeroSection;
