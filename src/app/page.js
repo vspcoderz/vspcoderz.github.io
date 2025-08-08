db408b28047cc12ecdab88425a85e4219b95f3ca
Vspcoder <165260098+vspcoderz@users.noreply.github.com>
2025-08-08 11:07:10 +0000

    less see this

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,14 @@
 import React, { useState } from "react";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
+import AboutMe from "@/components/AboutMe";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <AboutMe />
       
     </>
   );
